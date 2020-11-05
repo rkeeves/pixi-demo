c485701008c86413a88f87d82deed23e5759d379
rkeeves <aprettydecentaddress@gmail.com>
2020-11-05 22:42:23 +0100

    Added demo for PIXI

---
+++ src/main/java/com/rkeeves/pixidemo/PixiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rkeeves.pixidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkeeves/pixidemo/PixiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkeeves.pixidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
